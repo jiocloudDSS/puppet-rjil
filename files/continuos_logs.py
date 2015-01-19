83c9adfe6d7aaf0521c1676cfa1b0943dfac28ed
Gaurav Bafna <bafnag@gmail.com>
2015-01-19 18:26:54 +0000

    Removing the hardcoded params from the file.

--- files/continuos_logs.py
+++ files/continuos_logs.py
@@ -10,10 +10,13 @@ from key import *
 listing = glob.glob('/var/log/ceph/*.log')
 
 for files in listing:
-    filename=ntpath.basename(files)+"-"+socket.gethostname()
+    if "radosgw" in files:
+        filename=ntpath.basename(files)+"-"+socket.gethostname()
+    else:
+        filename=ntpath.basename(files)
     print filename
     print files
-    conn = boto.connect_s3(aws_access_key_id = access_key,aws_secret_access_key = secret_key,port=7480,debug=2,host = '10.140.214.196',          is_secure=False, calling_format = boto.s3.connection.OrdinaryCallingFormat(),)
+    conn = boto.connect_s3(aws_access_key_id = access_key,aws_secret_access_key = secret_key,port=port_number, debug=2,host = hostname,          is_secure=False, calling_format = boto.s3.connection.OrdinaryCallingFormat(),)
     bucket = conn.create_bucket('log_bucket')
     key = bucket.new_key(filename);
     key.set_contents_from_filename(files);
